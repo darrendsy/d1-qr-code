6d0630a64b6d76f5a7de9f1ca24c78d77f1a3334
SHUYAODAI <sdai8012@uni.sydney.edu.au>
2026-05-26 18:23:27 +1000

    Implement QR code service functions

---
+++ src/services/qrService.ts
@@ -0,0 +1,23 @@
+export async function getQrById(env, id) {
+  return await env.DB.prepare(
+    "SELECT * FROM qr_codes WHERE id = ?"
+  ).bind(id).first();
+}
+
+export async function getQrTarget(env, id) {
+  return await env.DB.prepare(
+    "SELECT target_url FROM qr_codes WHERE id = ?"
+  ).bind(id).first();
+}
+
+export async function createQr(env, id, targetUrl) {
+  return await env.DB.prepare(
+    "INSERT INTO qr_codes (id, target_url) VALUES (?, ?)"
+  ).bind(id, targetUrl).run();
+}
+
+export async function updateQr(env, id, targetUrl) {
+  return await env.DB.prepare(
+    "UPDATE qr_codes SET target_url = ? WHERE id = ?"
+  ).bind(targetUrl, id).run();
+}
