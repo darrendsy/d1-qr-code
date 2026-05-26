6bb09076d775e5ef5b08eefbabaf43f16f83ba74
SHUYAODAI <sdai8012@uni.sydney.edu.au>
2026-05-26 18:31:12 +1000

    Update qrService.ts

--- src/services/qrService.ts
+++ src/services/qrService.ts
@@ -4,6 +4,12 @@ export async function getQrById(env, id) {
   ).bind(id).first();
 }
 
+export async function getAllQrs(env) {
+  return await env.DB.prepare(
+    "SELECT * FROM qr_codes ORDER BY created_at DESC"
+  ).all();
+}
+
 export async function getQrTarget(env, id) {
   return await env.DB.prepare(
     "SELECT target_url FROM qr_codes WHERE id = ?"
